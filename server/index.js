8ae57b1839591639fb6aec768e3e461c36837332
Rodion Yermakov <rodion.yermakov.sd@gmail.com>
2024-06-17 15:47:05 -0700

    /ui/ shows the swagger documentation

--- server/index.js
+++ server/index.js
@@ -2,9 +2,15 @@
 
 var path = require('path');
 var http = require('http');
-
+var express = require('express');
 var oas3Tools = require('oas3-tools');
+var swaggerUi = require('swagger-ui-express');
+var fs = require('fs');
+var yaml = require('js-yaml');
 var serverPort = 8080;
+
+// Load the OpenAPI document
+var openApiDocument = yaml.load(fs.readFileSync(path.join(__dirname, 'api/openapi.yaml'), 'utf8'));
 
 // swaggerRouter configuration
 var options = {
@@ -14,11 +20,16 @@ var options = {
 };
 
 var expressAppConfig = oas3Tools.expressAppConfig(path.join(__dirname, 'api/openapi.yaml'), options);
-var app = expressAppConfig.getApp();
+var app = express();
+
+// Serve Swagger UI at /ui
+app.use('/ui', swaggerUi.serve, swaggerUi.setup(openApiDocument));
+
+// Use the oas3Tools app config middleware
+app.use(expressAppConfig.getApp());
 
 // Initialize the Swagger middleware
 http.createServer(app).listen(serverPort, function () {
     console.log('Your server is listening on port %d (http://localhost:%d)', serverPort, serverPort);
-    console.log('Swagger-ui is available on http://localhost:%d/docs', serverPort);
+    console.log('Swagger-ui is available on http://localhost:%d/ui', serverPort);
 });
-
